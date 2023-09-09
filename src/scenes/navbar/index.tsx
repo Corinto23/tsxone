2d39bde8399a95fd03043f513ccbafb4b6b69350
Corinto <salazar72390@gmail.com>
2023-09-09 14:38:34 +0800

    added settings

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
-import { SelectedPage } from "@/shared/types";
 type Props = {
   selectedPage: string;
   setSelectedPage: (value: string) => void;
